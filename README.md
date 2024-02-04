0c4cdc0ee1e0848f09c30cd0cabfdbfb5f89f8db
David Dong <hi@daviddong.me>
2024-02-04 10:58:45 +0800

    feat: virtual list

--- README.md
+++ README.md
@@ -4,18 +4,17 @@
 
 ## Todo
 
-- [x] 了解jsx
-    - [x] 可控性渲染
-    - [x] 模拟Babel解析JSX
-- [x] React组件
-    - [x] `<Component/>` 和 `Component()`的渲染区别
-    - [x] 静态属性
-    - [x] 组件通信之事件总线
-    - [x] HOC之属性代理
-    - [x] HOC之错误边界
-    - [x] 渲染分片
-    - [x] 组合模式
-    - [x] 事件循环
-    - [x] 改变渲染优先级
-    - [x] use-external-store
-    - [x] use-layout-effect
+- [x] 可控性渲染
+- [x] 模拟Babel解析JSX
+- [x] `<Component/>` 和 `Component()`的渲染区别
+- [x] 静态属性
+- [x] 组件通信之事件总线
+- [x] HOC之属性代理
+- [x] HOC之错误边界
+- [x] 渲染分片
+- [x] 组合模式
+- [x] 事件循环
+- [x] 改变渲染优先级
+- [x] use-external-store
+- [x] use-layout-effect
+- [x] 虚拟列表
--- src/pages/virtual-list.tsx
+++ src/pages/virtual-list.tsx
@@ -1,3 +1,93 @@
+//dev.to/adamklein/build-your-own-virtual-scroll-part-i-11ib
+
+import { memo, useEffect, useMemo, useRef, useState } from 'react'
+const MAX_ITEM_LENGTH = 100000
+const height = 300
+
 export default function VirtualList() {
-  return <div>VirtualList</div>
+  return (
+    <div className="bg-gray-100" style={{ height }}>
+      {/* NormalScroll 在渲染100000数据的时候很卡！ */}
+      {/* <NormalScroll /> */}
+      <VirtualScroll />
+    </div>
+  )
+}
+
+function useScrollAware() {
+  const [scrollTop, setScrollTop] = useState(0)
+  const ref = useRef<HTMLDivElement | null>(null)
+
+  const onScroll = (e: Event) =>
+    requestAnimationFrame(() => {
+      setScrollTop((e.target as HTMLElement).scrollTop)
+    })
+
+  useEffect(() => {
+    const scrollContainer = ref.current
+    setScrollTop(scrollContainer!.scrollTop)
+    scrollContainer!.addEventListener('scroll', onScroll)
+    return () => scrollContainer!.removeEventListener('scroll', onScroll)
+  }, [])
+  return { scrollTop, ref }
+}
+
+const Item = memo(({ index }: { index: number }) => (
+  <div
+    style={{
+      height: 30
+    }}
+    className="flex justify-between bg-gray-50 px-2"
+    key={index}>
+    row index {index}
+  </div>
+))
+
+// const NormalScroll = () => {
+//   const visibleChildren = useMemo(
+//     () => new Array(MAX_ITEM_LENGTH).fill(null).map((_, index) => <Item key={index} index={index} />),
+//     []
+//   )
+//   return (
+//     <div style={{ height: 300 }} className="overflow-y-scroll">
+//       {visibleChildren}
+//     </div>
+//   )
+// }
+const VirtualScroll = ({ itemCount = MAX_ITEM_LENGTH, height = 300, childHeight = 30, renderAhread = 20 }) => {
+  const { scrollTop, ref } = useScrollAware()
+  const totalHeight = itemCount * childHeight
+
+  let startNode = Math.floor(scrollTop / childHeight) - renderAhread
+  startNode = Math.max(0, startNode)
+
+  let visibleNodeCount = Math.ceil(height / childHeight) + 2 * renderAhread
+  visibleNodeCount = Math.min(itemCount - startNode, visibleNodeCount)
+  console.log(itemCount - startNode, visibleNodeCount)
+  const offsetY = startNode * childHeight
+
+  const visibleChildren = useMemo(
+    () =>
+      new Array(visibleNodeCount)
+        .fill(null)
+        .map((_, index) => <Item key={index + startNode} index={index + startNode} />),
+    [startNode, visibleNodeCount]
+  )
+
+  return (
+    <div style={{ height, overflow: 'auto' }} ref={ref}>
+      <div
+        style={{
+          height: totalHeight
+        }}>
+        <div
+          className="flex flex-col gap-y-4"
+          style={{
+            transform: `translateY(${offsetY}px)`
+          }}>
+          {visibleChildren}
+        </div>
+      </div>
+    </div>
+  )
 }
